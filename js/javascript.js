ef7055cce375dc34f18c0eeb8b36d8cae34af956
wangyat15 <114657394+wangyat15@users.noreply.github.com>
2023-03-14 15:59:52 +0800

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -2,14 +2,29 @@ function GetSelect1() {
   window.location.pathname = "searchforrs.html";
 }
 function GetSelect2() {
-  window.location.pathname = "updaters.html";
+  window.location.pathname = "viewrs.html";
 }
 function GetSelect3() {
-  window.location.pathname = "viewrs.html";
+  window.location.pathname = "updaters.html";
 }
 function GetSelect4() {
   window.location.pathname = "myridetrip.html";
 }
 function GetSelect5() {
   window.location.pathname = "myridetrip.html";
+}
+function GetSelect6() {
+  window.location.pathname = "searchforrt.html";
+}
+function GetSelect7() {
+  window.location.pathname = "viewrt.html";
+}
+function GetSelect8() {
+  window.location.pathname = "updatert.html";
+}
+function GetSelect9() {
+  window.location.pathname = "myrideshare.html";
+}
+function GetSelect10() {
+  window.location.pathname = "myrideshare.html";
 }
